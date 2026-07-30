b24c6ad0533379f8c6aae63ba93ca9c440e8724e
unluckypippo <filippoguerra1310@gmail.com>
2026-07-30 09:50:54 +0200

    fix: aggiunstamenti modulo userspace

--- src/modules/userspace.c
+++ src/modules/userspace.c
@@ -16,12 +16,18 @@
     ======================================
     thread in user mode senza accesso
     ______________________________________
-    r0/a1:  0x00000000  r1/a2:  0x00000000  r2/a3:  0x00000000
-    r3/a4:  0x00000000 r12/ip:  0x00000000 r14/lr:  0x00000000
-    xpsr:  0x00000000
-    Faulting instruction address (r15/pc): 0x00000000
-
-
+    [00:00:00.000,000] ␛[1;31m<err> os: thread 0x200005d8 (1) does not have permission on k_sem 0x2000020c␛[0m
+    [00:00:00.000,000] ␛[1;31m<err> os: permission bitmap
+                                00 00                                            |..               ␛[0m
+    [00:00:00.000,000] ␛[1;31m<err> os: syscall z_vrfy_k_sem_take failed check: access denied␛[0m
+    [00:00:00.000,000] ␛[1;31m<err> os: r0/a1:  0x00000000  r1/a2:  0x00000000  r2/a3:  0x00000000␛[0m
+    [00:00:00.000,000] ␛[1;31m<err> os: r3/a4:  0x00000000 r12/ip:  0x00000000 r14/lr:  0x00000000␛[0m
+    [00:00:00.000,000] ␛[1;31m<err> os:  xpsr:  0x00000000␛[0m
+    [00:00:00.000,000] ␛[1;31m<err> os: Faulting instruction address (r15/pc): 0x0800d55f␛[0m
+    [00:00:00.000,000] ␛[1;31m<err> os: >>> ZEPHYR FATAL ERROR 3: Kernel oops on CPU 0␛[0m
+    [00:00:00.000,000] ␛[1;31m<err> os: Current thread: 0x200005d8 (unknown)␛[0m
+    [00:00:00.032,000] ␛[1;31m<err> os: Halting system
+    ␛[0m
     ============================================================
 */
 
@@ -33,7 +39,6 @@
 
 K_SEM_DEFINE(sem, 0, 1);
 #define STACK_SIZE 1024
-#define PRIORITY 5
 
 static void user_thread_1 (void *p1, void *p2, void *p3){
     printk("thread in user mode con accesso\n");
@@ -48,8 +53,8 @@ static void user_thread_2 (void *p1, void *p2, void *p3){
     k_sem_take(&sem, K_FOREVER);
 }
 
-K_THREAD_DEFINE(user_thread_id_1, STACK_SIZE, user_thread_1, NULL, NULL, NULL, PRIORITY, K_USER, 0);
-K_THREAD_DEFINE(user_thread_id_2, STACK_SIZE, user_thread_2, NULL, NULL, NULL, PRIORITY, K_USER, 0);
+K_THREAD_DEFINE(user_thread_id_1, STACK_SIZE, user_thread_1, NULL, NULL, NULL, 5, K_USER, 0);
+K_THREAD_DEFINE(user_thread_id_2, STACK_SIZE, user_thread_2, NULL, NULL, NULL, 6, K_USER, 0);
 
 void module_user_run(void){
     printk("Modulo UserSpace attivo: Test accesso kernel object da user thread\n");
